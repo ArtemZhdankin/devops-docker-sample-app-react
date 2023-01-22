802427dbbfb5e1a30b5e168aa9a5a5a41b2db7e1
ArtemZhdankin <106024923+ArtemZhdankin@users.noreply.github.com>
2023-01-22 20:17:09 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>Artem Zhdankin is a frontend developer and just an awesome man.</p>
+    </div>
+  );
+}
+
+export default App;
